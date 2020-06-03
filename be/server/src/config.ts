ecd17a171ac8fd4abd15cd2dcb71089a0f581b7e
Ryan Ling <ryan@outlook.com.au>
2020-06-03 17:35:25 +1000

    Comment on the dangerous environment variable

--- be/server/src/config.ts
+++ be/server/src/config.ts
@@ -4,5 +4,9 @@ export const FE_ORIGIN = 'http://dev.seekunifiedbeta.com:8080';
 
 export const USER_AGENT = 'wingman/local';
 
+// Take care when handling your token; it’s easy for environment variables to be
+// accessed by other processes, accidentally logged, or persisted to shell
+// history. We recommend wiring this up to a secret store like AWS Secrets
+// Manager.
 export const getPartnerToken = () =>
   Promise.resolve(process.env.DANGEROUS_PLAYGROUND_TOKEN ?? '');
