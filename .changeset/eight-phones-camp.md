b307e2f6e89170d309122f2bae0a1a1969301cf4
Aaron Vanston <aaronvanston@users.noreply.github.com>
2020-06-05 15:29:16 +1000

    feat: Add SEEK graphql types (#36)
    * feat: add codegen to pull in seek graph schema
    
    * feat: add exports to seek types within fe/lib
    
    * chore: add changeset

---
+++ .changeset/eight-phones-camp.md
@@ -0,0 +1,5 @@
+---
+'wingman-fe': minor
+---
+
+Exports SEEK graphql types for consumer use
---
+++ types/seekFragmentTypes.ts
@@ -0,0 +1,53 @@
+
+      export interface IntrospectionResultData {
+        __schema: {
+          types: {
+            kind: string;
+            name: string;
+            possibleTypes: {
+              name: string;
+            }[];
+          }[];
+        };
+      }
+      const result: IntrospectionResultData = {
+  "__schema": {
+    "types": [
+      {
+        "kind": "INTERFACE",
+        "name": "ApplicationQuestionnaireComponent",
+        "possibleTypes": [
+          {
+            "name": "ApplicationQuestion"
+          },
+          {
+            "name": "ApplicationPrivacyConsent"
+          }
+        ]
+      },
+      {
+        "kind": "INTERFACE",
+        "name": "ApplicationQuestionnaireComponentResponse",
+        "possibleTypes": [
+          {
+            "name": "ApplicationQuestionResponse"
+          },
+          {
+            "name": "ApplicationPrivacyConsentResponse"
+          }
+        ]
+      },
+      {
+        "kind": "INTERFACE",
+        "name": "Event",
+        "possibleTypes": [
+          {
+            "name": "CandidateApplicationCreatedEvent"
+          }
+        ]
+      }
+    ]
+  }
+};
+      export default result;
+    
