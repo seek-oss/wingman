98d5f88ecb3862e0d27f2d3dbb6f83b558528c52
Ryan Ling <ryan@outlook.com.au>
2020-06-07 12:36:26 +1000

    Add root middleware to example backend (#44)
    This bundles some basic error handling and request logging together.

---
+++ be/example/src/api/root.ts
@@ -0,0 +1,27 @@
+import { Middleware } from 'koa';
+
+const isObject = (value: unknown): value is Record<PropertyKey, unknown> =>
+  typeof value === 'object' && value !== null;
+
+export const rootMiddleware: Middleware = async (ctx, next) => {
+  try {
+    await next();
+
+    /* eslint-disable-next-line no-console */
+    console.log(ctx.method, ctx.url, ctx.status);
+  } catch (anyErr) {
+    const err = anyErr as unknown;
+
+    /* eslint-disable-next-line no-console */
+    console.error(ctx.method, ctx.url, ctx.status, err);
+
+    if (!isObject(err) || typeof err.status !== 'number') {
+      ctx.status = 500;
+      ctx.body = '';
+      return;
+    }
+
+    ctx.status = err.status;
+    ctx.body = (err.status < 500 && err.message) || '';
+  }
+};
