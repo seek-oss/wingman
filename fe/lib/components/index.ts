469f181a492b6c0b01211c7e50a962e9ae852453
Lumi <lumi.shrestha@gmail.com>
2020-07-10 11:42:22 +1000

    Export Questionnaire builder components

--- fe/lib/components/index.ts
+++ fe/lib/components/index.ts
@@ -2,3 +2,20 @@ export { Example } from './Example/Example';
 export { JobCategorySelect } from './JobCategorySelect/JobCategorySelect';
 export { JobCategorySuggest } from './JobCategorySuggest/JobCategorySuggest';
 export { LocationSuggest } from './LocationSuggest/LocationSuggest';
+
+export { QuestionnaireBuilder } from './QuestionnaireBuilder/QuestionnaireBuilder/QuestionnaireBuilder';
+
+export { FormBuilder } from './QuestionnaireBuilder/QuestionnaireBuilder/FormBuilder/FormBuilder';
+export { FormRenderer } from './QuestionnaireBuilder/QuestionnaireBuilder/FormRenderer/FormRenderer';
+export {
+  GraphqlQueryRenderer,
+  MutationVariables,
+} from './QuestionnaireBuilder/components/GraphqlQueryRenderer/GraphqlQueryRenderer';
+
+export {
+  Question,
+  FreeTextQuestion,
+  SelectionQuestion,
+  FormComponent,
+  PrivacyConsent,
+} from './QuestionnaireBuilder/questionTypes';
