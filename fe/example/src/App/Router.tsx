434c36db5694987882f7fabddeaebdb075f651ca
Ryan Ling <ryan@outlook.com.au>
2020-07-21 13:05:09 +1000

    Add position list and detail pages (#127)

--- fe/example/src/App/Router.tsx
+++ fe/example/src/App/Router.tsx
@@ -7,6 +7,8 @@ import { AdminPage } from '../pages/Admin/Admin';
 import { CandidateDetailPage } from '../pages/Candidates/Detail';
 import { CandidateListPage } from '../pages/Candidates/List';
 import { OopsPage } from '../pages/Oops';
+import { PositionDetailPage } from '../pages/Positions/Detail';
+import { PositionListPage } from '../pages/Positions/List';
 import { PositionNewPage } from '../pages/Positions/New';
 
 export const Router = () => (
@@ -20,9 +22,16 @@ export const Router = () => (
     <Route exact path="/candidates/detail/:id">
       <CandidateDetailPage />
     </Route>
+    <Route exact path="/positions">
+      <PositionListPage />
+    </Route>
+    <Route exact path="/positions/detail/:id">
+      <PositionDetailPage />
+    </Route>
     <Route exact path="/positions/new">
       <PositionNewPage />
     </Route>
+
     <Route path="/storybook">
       <Redirect to="https://seek-oss.github.io/wingman/storybook/index.html" />
     </Route>
---
+++ fe/example/src/pages/Ads/List.tsx
@@ -0,0 +1,178 @@
+import {
+  Actions,
+  Alert,
+  Badge,
+  Box,
+  Button,
+  Card,
+  Column,
+  Columns,
+  Heading,
+  IconAdd,
+  IconEdit,
+  IconLocation,
+  IconSubCategory,
+  IconTime,
+  Inline,
+  Stack,
+  Text,
+} from 'braid-design-system';
+import React, { useState } from 'react';
+import { useStyles } from 'sku/react-treat';
+import { StringParam, useQueryParam } from 'use-query-params';
+
+import { Ad } from '../../data/ads';
+import { NewAdForm } from '../../widgets/NewAdForm';
+
+import * as styleRefs from './List.treat';
+
+interface Props {
+  ads: Ad[];
+  position: string;
+}
+
+export const AdList = ({ ads }: Props) => {
+  const [newAd, setNewAd] = useState(false);
+
+  const [selectedAdId, setSelectedAdId] = useQueryParam('ad', StringParam);
+
+  const [editAds, setEditAds] = useState(
+    [...new Array(ads.length)].map(() => false),
+  );
+
+  const styles = useStyles(styleRefs);
+
+  return (
+    <Box padding="gutter">
+      <Stack space="gutter">
+        {ads.length ? undefined : (
+          <Alert tone="info">
+            <Text>You haven’t advertised this position yet.</Text>
+          </Alert>
+        )}
+
+        {ads.map((ad, index) => (
+          <Box
+            className={{
+              [styles.ad]: true,
+              [styles.selectedAd]: ad.id === selectedAdId,
+            }}
+            key={index}
+            onClick={() => setSelectedAdId(ad.id)}
+          >
+            {editAds[index] ? (
+              <Stack dividers space="none">
+                <Card>
+                  <Heading level="4">Edit ad</Heading>
+                </Card>
+
+                {/* TODO: avoid sharing the same local storage state */}
+                <NewAdForm initial={ad} />
+
+                <Card>
+                  {/* TODO: actually do something */}
+                  <Actions>
+                    <Button
+                      onClick={() =>
+                        setEditAds((array) => {
+                          array.splice(index, 1, false);
+                          return [...array];
+                        })
+                      }
+                    >
+                      Save
+                    </Button>
+                    <Button
+                      onClick={() =>
+                        setEditAds((array) => {
+                          array.splice(index, 1, false);
+                          return [...array];
+                        })
+                      }
+                      weight="weak"
+                    >
+                      Discard
+                    </Button>
+                  </Actions>
+                </Card>
+              </Stack>
+            ) : (
+              <Card>
+                <Columns space="gutter">
+                  <Column>
+                    <Stack space="gutter">
+                      <Inline space="small" alignY="center">
+                        <Heading level="4">{ad.title}</Heading>
+
+                        <Badge
+                          tone={ad.adProduct === 'Premium' ? 'promote' : 'info'}
+                        >
+                          {ad.adProduct}
+                        </Badge>
+                      </Inline>
+
+                      <Inline space="small" alignY="center">
+                        <Text>
+                          <IconLocation /> {ad.location}
+                        </Text>
+                        <Text>
+                          <IconSubCategory /> {ad.category}
+                        </Text>
+                      </Inline>
+
+                      <Text>{ad.summary}</Text>
+                    </Stack>
+                  </Column>
+                  <Column width="content">
+                    <Actions>
+                      <Button
+                        onClick={() =>
+                          setEditAds((array) => {
+                            array.splice(index, 1, true);
+                            return [...array];
+                          })
+                        }
+                      >
+                        <IconEdit /> Edit
+                      </Button>
+                      <Button>
+                        <IconTime /> Expire
+                      </Button>
+                    </Actions>
+                  </Column>
+                </Columns>
+              </Card>
+            )}
+          </Box>
+        ))}
+
+        {newAd ? (
+          <Stack dividers space="none">
+            <Card>
+              <Heading level="4">New ad</Heading>
+            </Card>
+
+            {/* TODO: avoid sharing the same local storage state */}
+            <NewAdForm />
+
+            <Card>
+              {/* TODO: actually do something */}
+              <Actions>
+                <Button onClick={() => setNewAd(false)}>Post</Button>
+                <Button onClick={() => setNewAd(false)} weight="weak">
+                  Discard
+                </Button>
+              </Actions>
+            </Card>
+          </Stack>
+        ) : (
+          <Actions>
+            <Button onClick={() => setNewAd(true)}>
+              <IconAdd /> New
+            </Button>
+          </Actions>
+        )}
+      </Stack>
+    </Box>
+  );
+};
