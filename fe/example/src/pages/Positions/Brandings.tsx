9f8ed6fbf1caff160de5e60f02785a7ee417bce5
Ryan Ling <ryan@outlook.com.au>
2020-07-21 13:14:42 +1000

    Add branding and questionnaire page stubs
    These should eventually become proper list views with edits, deletes and
    all that jazz.
    
    Co-authored-by: Lumi Shrestha <lumi.shrestha@gmail.com>

---
+++ fe/example/src/pages/Positions/Brandings.tsx
@@ -0,0 +1,32 @@
+import { Box, ContentBlock, Heading, Stack, Text } from 'braid-design-system';
+import React, { useState } from 'react';
+
+import { Header } from '../../components/Header';
+import { Branding } from '../../data/brandings';
+import { BrandingSelect } from '../../widgets/BrandingSelect';
+
+export const PositionBrandingPage = () => {
+  const [selection, setSelection] = useState<Branding>();
+
+  return (
+    <Stack dividers space="none">
+      <Header>
+        <Heading level="3">Brandings</Heading>
+      </Header>
+
+      <ContentBlock width="large">
+        <Box padding="gutter">
+          <Stack space="gutter">
+            <Text>
+              {selection
+                ? `Branding ID ${selection.id}`
+                : 'No branding selected'}
+            </Text>
+
+            <BrandingSelect onChange={setSelection} />
+          </Stack>
+        </Box>
+      </ContentBlock>
+    </Stack>
+  );
+};
