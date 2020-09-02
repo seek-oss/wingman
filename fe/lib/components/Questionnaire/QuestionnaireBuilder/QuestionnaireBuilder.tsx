015535902f1d4d4dbfd2981facab69d9030e3539
Ryan Cumming <etaoins@gmail.com>
2020-09-02 17:43:54 +1000

    Remove hirer ID from the questionnaire builder
    This is currently broken unless a mutation is provided. Instead, this
    depends on our consumers supplying their own ID. This can either be
    provided from their session state or from an explicit input.

--- fe/lib/components/Questionnaire/QuestionnaireBuilder/QuestionnaireBuilder.tsx
+++ fe/lib/components/Questionnaire/QuestionnaireBuilder/QuestionnaireBuilder.tsx
@@ -7,7 +7,6 @@ import {
   ContentBlock,
   Heading,
   Stack,
-  TextField,
 } from 'braid-design-system';
 import React, { useEffect, useState } from 'react';
 
@@ -23,23 +22,21 @@ import { FormComponent, MutationVariableInput } from '../questionTypes';
 import { FormBuilder } from './FormBuilder/FormBuilder';
 
 interface QuestionnaireBuilderProps {
+  hirerId: string;
   graphqlInput?: MutationVariableInput;
   onChange?: (mutationVariables: QuestionnaireCreateInput) => void;
 }
 
 export const QuestionnaireBuilder = ({
+  hirerId,
   graphqlInput,
   onChange,
 }: QuestionnaireBuilderProps) => {
   const [formBuilderState, setFormBuilderState] = useState<FormComponent[]>([]);
-  const [hirerId, setHirerId] = useState(
-    graphqlInput?.input.applicationQuestionnaire.hirerId ?? '',
-  );
 
   useEffect(() => {
     if (graphqlInput) {
       setFormBuilderState(mapMutationVariableToFormComponent(graphqlInput));
-      setHirerId(graphqlInput.input.applicationQuestionnaire.hirerId);
     }
   }, [graphqlInput]);
 
@@ -71,16 +68,6 @@ export const QuestionnaireBuilder = ({
         <Card>
           <Stack space="large">
             <Heading level="3">GraphQL Output</Heading>
-
-            {typeof hirerId === 'undefined' && (
-              <TextField
-                id="hirerId"
-                label="Hirer OID"
-                placeholder="seekAnzPublicTest:organization:seek:93WyyF1h"
-                value={hirerId}
-                onChange={({ currentTarget: { value } }) => setHirerId(value)}
-              />
-            )}
 
             <GraphqlQueryRenderer
               components={formBuilderState}
