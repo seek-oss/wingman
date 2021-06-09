c74d6a7bc96f9ac72ef0490b5b72e3d3798cf1ea
Ryan Cumming <etaoins@gmail.com>
2021-06-10 07:04:12 +1000

    fix: Reformat for prettier & relock

--- be/src/browserToken/middleware.test.ts
+++ be/src/browserToken/middleware.test.ts
@@ -15,11 +15,10 @@ import {
 import { createBrowserTokenMiddleware, resetTokenCache } from './middleware';
 
 describe('createBrowserTokenMiddleware', () => {
-  const getPartnerToken =
-    jest.fn<
-      Promise<{ hirerId: string; partnerToken: string }>,
-      [RetrieveRequest]
-    >();
+  const getPartnerToken = jest.fn<
+    Promise<{ hirerId: string; partnerToken: string }>,
+    [RetrieveRequest]
+  >();
 
   const agent = createAgent(() => {
     const middleware = createBrowserTokenMiddleware({
