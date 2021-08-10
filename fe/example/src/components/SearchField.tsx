fc1739be360bae5b99a3db1b42939b21618e86dc
Ryan Ling <ryan@outlook.com.au>
2021-08-11 09:15:37 +1000

    Fix type badness

--- fe/example/src/components/SearchField.tsx
+++ fe/example/src/components/SearchField.tsx
@@ -3,8 +3,10 @@ import React, { ComponentProps, useRef } from 'react';
 
 type Props = Pick<
   ComponentProps<typeof TextField>,
-  'aria-label' | 'autoFocus' | 'id' | 'onChange' | 'placeholder' | 'value'
->;
+  'autoFocus' | 'id' | 'onChange' | 'placeholder' | 'value'
+> & {
+  'aria-label': string;
+};
 
 export const SearchField = (props: Props) => {
   const ref = useRef<HTMLInputElement>(null);
