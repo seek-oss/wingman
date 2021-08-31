f5ffa36a2c02120ab4e3e250dcc4ecbe213cafbf
Ryan Ling <ryan@outlook.com.au>
2021-08-31 10:38:58 +1000

    Add MockAdSelectionFallback (#552)
    This is a simple Storybook action wrapper around the AdSelectionFallback
    component. There's nothing particularly exciting to see in Storybook,
    but it was easy enough to include this for alignment with other widgets.

---
+++ .changeset/tidy-plants-speak.md
@@ -0,0 +1,5 @@
+---
+'wingman-fe': minor
+---
+
+**MockAdSelectionFallback:** Add component
--- fe/lib/components/AdSelectionFallback/AdSelectionFallback.stories.tsx
+++ fe/lib/components/AdSelectionFallback/AdSelectionFallback.stories.tsx
@@ -2,18 +2,25 @@ import 'braid-design-system/reset';
 
 import React, { ComponentProps } from 'react';
 
-import { BraidArgs, defaultArgTypes } from '../../storybook/controls';
+import {
+  BraidArgs,
+  defaultArgTypes,
+  defaultArgs,
+} from '../../storybook/controls';
 import { BraidStorybookProvider } from '../../storybook/decorators';
 
 import { AdSelectionFallback as Component } from './AdSelectionFallback';
+import { MockAdSelectionFallback } from './AdSelectionFallback.mock';
 
 export default {
   args: {
+    braidThemeName: defaultArgs.braidThemeName,
     id: 'seek-advertisement-type',
   },
   argTypes: {
     braidThemeName: defaultArgTypes.braidThemeName,
     onSelect: { action: 'onSelect' },
+    showStorybookAction: defaultArgTypes.showStorybookAction,
   },
   component: Component,
   title: 'Job Posting/Ad selection/AdSelectionFallback',
@@ -23,7 +30,7 @@ type Args = ComponentProps<typeof Component> & BraidArgs;
 
 export const AdSelectionFallback = ({ braidThemeName, ...args }: Args) => (
   <BraidStorybookProvider braidThemeName={braidThemeName}>
-    <Component {...args} />
+    <MockAdSelectionFallback {...args} />
   </BraidStorybookProvider>
 );
 AdSelectionFallback.storyName = 'AdSelectionFallback';
