f4ec8ada12aad0daa25d5b2e7755850f53f93589
Stella Hu <shu@seek.com.au>
2022-05-11 16:35:27 +1000

    fix

--- fe/lib/components/LocationLookup/__fixtures__/location.ts
+++ fe/lib/components/LocationLookup/__fixtures__/location.ts
@@ -1,13 +1,41 @@
 export const mockLocation = {
+  id: {
+    value: 'seekAnz:location:seek:2FqwWaaMV',
+  },
+  name: 'Melbourne',
+  contextualName: 'Melbourne VIC 3000 AU',
+  countryCode: 'AU',
   parent: {
     id: {
-      value: 'seekAnz:jobCategory:seek:2WejkktVM',
+      value: 'seekAnz:location:seek:2m81wybwV',
     },
-    name: 'Accounting',
+    name: 'CBD & Inner Suburbs',
+    contextualName: 'CBD & Inner Suburbs, Melbourne VIC AU',
+    countryCode: 'AU',
+    parent: {
+      id: {
+        value: 'seekAnz:location:seek:31XoHiay5',
+      },
+      name: 'Melbourne',
+      contextualName: 'Melbourne VIC AU',
+      countryCode: 'AU',
+      parent: {
+        id: {
+          value: 'seekAnz:location:seek:HxMS1gfR',
+        },
+        name: 'Victoria',
+        contextualName: 'Victoria AU',
+        countryCode: 'AU',
+        parent: {
+          id: {
+            value: 'seekAnz:location:seek:2aeax6diF',
+          },
+          name: 'Australia',
+          contextualName: 'Australia',
+          countryCode: 'AU',
+          parent: null,
+        },
+      },
+    },
   },
-  children: null,
-  id: {
-    value: 'seekAnz:jobCategory:seek:CTriSTrf',
-  },
-  name: 'Accounts Payable',
 };
