09f132409a1e0e112579ce0a4681870a7f9383fb
Tomas Bolger <tbolger.business@gmail.com>
2022-10-18 09:06:13 +1100

    Update fe/lib/utils/formatPoint.ts
    Co-authored-by: Ryan Ling <ryan@outlook.com.au>

--- fe/lib/utils/formatPoint.ts
+++ fe/lib/utils/formatPoint.ts
@@ -1,7 +1,7 @@
 export const formatPoint = (point: [number, number]): string => {
-  const roundedLat = Math.abs(Math.round(point[0] * 100) / 100);
-  const roundedLon = Math.abs(Math.round(point[1] * 100) / 100);
-  const lat = roundedLat < 0 ? `${roundedLat}° S` : `${roundedLat}° N`;
-  const lon = roundedLon < 0 ? `${roundedLon}° W` : `${roundedLon}° E`;
+  const roundedLat = Math.abs(point[0]).toFixed(2);
+  const roundedLon = Math.abs(point[1]).toFixed(2);
+  const lat = point[0] < 0 ? `${roundedLat}° S` : `${roundedLat}° N`;
+  const lon = point[1] < 0 ? `${roundedLon}° W` : `${roundedLon}° E`;
   return `${lat}, ${lon}`;
 };
