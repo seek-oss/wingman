b337787790ee32526517b7233101a0a164a2b422
Ryan Ling <ryan@outlook.com.au>
2022-11-17 12:38:21 +1100

    Upgrade to Scoobie 14
    https://github.com/seek-oss/scoobie/releases/tag/v14.0.0

---
+++ .changeset/famous-flies-argue.md
@@ -0,0 +1,5 @@
+---
+'wingman-fe': major
+---
+
+Require Scoobie 14+
--- fe/lib/private/CopyableOid.tsx
+++ fe/lib/private/CopyableOid.tsx
@@ -1,23 +1,10 @@
-import { IconCopy, IconTick } from 'braid-design-system';
-import React, { ComponentProps, Fragment } from 'react';
+import React, { ComponentProps } from 'react';
 import { CopyableText } from 'scoobie';
 
 type Props = Pick<ComponentProps<typeof CopyableText>, 'children' | 'size'>;
 
 export const CopyableOid = ({ children, size }: Props) => (
-  <CopyableText
-    copiedLabel={
-      <Fragment>
-        <IconTick alignY="lowercase" /> Copied OID
-      </Fragment>
-    }
-    copyLabel={
-      <Fragment>
-        <IconCopy alignY="lowercase" /> Copy OID
-      </Fragment>
-    }
-    size={size}
-  >
+  <CopyableText copiedLabel="Copied OID" copyLabel="Copy OID" size={size}>
     {children}
   </CopyableText>
 );
