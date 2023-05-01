95dc1cad7cc95877519cb30bd7b0bc32c447f221
Ryan Ling <ryan@outlook.com.au>
2023-05-01 10:39:48 +1000

    fe: Fix compilation error (#923)
    * fe: Fix compilation error
    
    Resolves a regression from #917 due to formatting differences in GraphQL
    directive descriptions. `@graphql-tools/utils` does not round-trip block
    formatting.
    
    https://github.com/ardatan/graphql-tools/blob/%40graphql-tools/utils%409.2.1/packages/utils/src/print-schema-with-directives.ts#L205-L211
    
    * Fix loop jump statement
    
    * Tweak comment

--- typescript-typedefs.js
+++ typescript-typedefs.js
@@ -3,18 +3,49 @@
  */
 
 const { printSchemaWithDirectives } = require('@graphql-tools/utils');
+const { Kind } = require('graphql');
 
-const printTypeScriptTypeDefs = (schema) => `
+/**
+ * @param {import('graphql').GraphQLSchema} schema
+ */
+const forceBlockDescriptionsOnDirectives = (schema) => {
+  const directives = schema.getDirectives();
+
+  for (const directive of directives) {
+    if (directive.astNode || !directive.description) {
+      continue;
+    }
+
+    directive.astNode = {
+      kind: Kind.DIRECTIVE_DEFINITION,
+      // Use block styling to ensure that our hack in `printTypeScriptTypeDefs`
+      // to escape backticks still outputs valid GraphQL type defs.
+      description: {
+        kind: Kind.STRING,
+        value: directive.description,
+        block: true,
+      },
+    };
+  }
+};
+
+/**
+ * @param {string} source
+ */
+const printTypeScriptTypeDefs = (source) => `
 import gql from 'graphql-tag';
 
 export const typeDefs = gql\`
-${schema.replace(/`/g, '\\`')}
+${source.replace(/`/g, '\\`')}
 \`;
 `;
 
 module.exports = {
-  plugin: (schema) =>
-    printTypeScriptTypeDefs(
-      printSchemaWithDirectives(schema),
-    ),
+  plugin: (schema) => {
+    forceBlockDescriptionsOnDirectives(schema);
+
+    const source = printSchemaWithDirectives(schema);
+
+    return printTypeScriptTypeDefs(source);
+  },
 };
